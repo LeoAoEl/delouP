b866ab9b17ff8e17a5f9823c1024cdaf4f33877f
Leo0114 <leonel0614@gmail.com>
2024-07-08 03:18:57 -0600

    ft:HoverElements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,14 @@ export default {
         footerBg: "#542c8c",
         footerAcentuate: "#502889",
         par: "#fbf1e5",
+        vsColor: "#fef1e6",
+        naranja: "#fc7d2e",
+      },
+      backgroundImage: {
+        heroMain:
+          "linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.0)), url('/src/assets/images/1.webp')",
+        waves: " url('/src/assets/images/wave.webp')",
+        bottomWave: " url('/src/assets/images/purpleWave.webp')",
       },
     },
   },
